ff683dff00d01cf7d1f8c9acde23007d08d1f2fe
JPAM0898 <jpaguirrem@unal.edu.co>
2021-11-17 22:24:56 -0500

    Inicio Front

---
+++ front/pages/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Login = () => {
+  return (
+    <div>
+      <Layout>
+        <h1 className="text-center text-2xl text-white font-bold">Inicio de sesión</h1>
+      </Layout>
+    </div>
+  );
+};
+export default Login;
---
+++ front/pages/productos.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+
+const Productos = () => (
+  <div>
+    <Layout>
+      <h1 className="text-2xl text-white font-light">Productos</h1>
+    </Layout>
+  </div>
+);
+export default Productos;
+  
