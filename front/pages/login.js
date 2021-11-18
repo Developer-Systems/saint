5833db4b4bd50ea34d809e058a9d81f097aa07fc
Santiago Pinilla <santiago.pinilla.45672@gmail.com>
2021-11-18 17:33:44 -0500

    update signup.js
    front crear nueva cuenta

--- front/pages/login.js
+++ front/pages/login.js
@@ -10,7 +10,7 @@ const Login = () => {
           <div className="w-full max-w-sm">
             <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
               <div className= "mb-4">
-                <label className="block text-gray-700 text-sm  font-bold mb-2" htmlfor="nombre">
+                <label className="block text-gray-700 text-sm  font-bold mb-2" htmlfor="email">
                   Email
                 </label>    
                 <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus: outline-none focus:shadow-outline  "  id="email" type="email" placeholder="Email Usuario" />
