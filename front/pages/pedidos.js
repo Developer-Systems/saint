361dbde9ac3f46fe4a3259839b758e1fe06e0575
VivianaC <vivi_c11@yahoo.es>
2021-11-28 18:04:58 -0500

    Creación de componente para asignar productos, agregación de productos y Creación de nuevos pedidos, Agregación de ReactSelect, Creación de componente para asignar cliente, Configuración de useReduce para manejo de Pedidos, agragación de clientes

--- front/pages/pedidos.js
+++ front/pages/pedidos.js
@@ -1,10 +1,16 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
+import Link from 'next/Link';
+
 
 const Pedidos = () => (
   <div>
     <Layout>
       <h1 className="text-2xl text-white font-light">Pedidos</h1>
+
+      <Link href="/nuevopedido">  
+        <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white round text-sm hover:bg-gray-800 mb-3 uppercase font-bold "> Nuevo Pedido</a>
+      </Link> 
     </Layout>
   </div>
 );
