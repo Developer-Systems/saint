e822d0db54c9d0adcebfe5dfea0728d5c6fc9522
CrackMayo <tamayo998@hotmail.com>
2021-12-05 18:36:16 -0500

    Se completa simpleBarChar de acuerdo con los mejores vendedores

--- front/pages/mejores-vendedores.js
+++ front/pages/mejores-vendedores.js
@@ -11,53 +11,43 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
+import { gql, useQuery} from '@apollo/client';
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+const MEJORES_VENDEDORES = gql`
+query MejoresVendedores {
+  mejoresVendedores {
+    total
+    vendedor {
+      nombre
+      email
+    }
+  }
+}`;
+
 
 const MejoresVendedores = () => {
+
+  const {data, loading, error} = useQuery(MEJORES_VENDEDORES);
+  console.log(data);
+  console.log(loading);
+  console.log(error);
+
+ if(loading) return 'cargando...';
+ console.log(data.mejoresVendedores);
+
+ const {mejoresVendedores} = data;
+
+ const vendedorGrafica = [];
+
+ mejoresVendedores.map((vendedor, index) => {
+   vendedorGrafica[index] = {
+     ...vendedor.vendedor[0],
+     total: vendedor.total
+   }
+ });
+
+ console.log(vendedorGrafica);
+
   return (
     <Layout>
       <h1 className="text-2xl text-white font-light">Mejores Vendedores</h1>
@@ -66,7 +56,7 @@ const MejoresVendedores = () => {
         className="mt-10"
         width={600}
         height={500}
-        data={data}
+        data={vendedorGrafica}
         margin={{
           top: 5,
           right: 30,
@@ -75,11 +65,11 @@ const MejoresVendedores = () => {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="nombre" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="pv" fill="#3182CE" />
+        <Bar dataKey="total" fill="#3182CE" />
       </BarChart>
     </Layout>
   );
